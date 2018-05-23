a46f11d19fee819160fa5be2a15e55301c5f8984
Stacy Carr <stacy@boostersinabox.com>
2018-05-23 20:22:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,13 @@
 class Dog 
- def name=(dog_name)
-   name = dog_name
- end
- 
- def name
-   @name
- end
-  
+  def initialize(name)
+    @name = name
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
   def bark
     puts "woof!"
   end
